6e6ecc686f11c6424220829a4aae6c82ea80b8e6
Jon Onstott <jon@eaglecapsoftware.com>
2019-04-09 15:40:46 -0700

    Add the ability to parse user input

---
+++ src/main/java/onstott/refractions/util/RefractionInputValidator.java
@@ -0,0 +1,38 @@
+package onstott.refractions.util;
+
+/* Class which converts user input to numeric values, if possible */
+public class RefractionInputValidator {
+
+    public double parseSpherePower(String input) {
+        if (input.length() == 0) {
+            throw new IllegalArgumentException("Please enter a value for Sphere power");
+        }
+        double spherePower = Double.parseDouble(input);
+        if (spherePower < -25 || spherePower > 25) {
+            throw new NumberFormatException("Sphere power must be in the range of -25 to 25");
+        }
+        return spherePower;
+    }
+
+    public double parseCylinderPower(String input) {
+        if (input.length() == 0) {
+            throw new IllegalArgumentException("Please enter a value for Cylinder power");
+        }
+        double cylinderPower = Double.parseDouble(input);
+        if (cylinderPower < -15 || cylinderPower > 15) {
+            throw new NumberFormatException("Cylinder power must be in the range of -15 to 15");
+        }
+        return cylinderPower;
+    }
+
+    public int parseAxis(String input) {
+        if (input.length() == 0) {
+            throw new IllegalArgumentException("Please enter a value for Axis");
+        }
+        if (!input.toLowerCase().startsWith("x")) {
+            throw new IllegalArgumentException("Axis must begin with an 'x'");
+        }
+        return Integer.parseInt(input.substring(1));
+    }
+
+}
---
+++ src/test/java/onstott/refractions/util/RefractionInputValidatorTest.java
@@ -0,0 +1,52 @@
+package onstott.refractions.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RefractionInputValidatorTest {
+
+    private RefractionInputValidator validator = new RefractionInputValidator();
+
+    @Test
+    public void parseValidSpherePower() {
+        Assert.assertEquals(1.25, this.validator.parseSpherePower("1.25"), 0);
+        Assert.assertEquals(12, this.validator.parseSpherePower("12.00"), 0);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void parseInvalidSpherePower() {
+        this.validator.parseSpherePower("abc");
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void parseOutOfRangeSpherePower() {
+        this.validator.parseSpherePower("100");
+    }
+
+    @Test
+    public void parseValidCylinderPower() {
+        Assert.assertEquals(0, this.validator.parseCylinderPower("0.00"), 0);
+        Assert.assertEquals(10, this.validator.parseCylinderPower("10.00"), 0);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void parseInvalidCylinderPower() {
+        this.validator.parseCylinderPower("abc");
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void parseOutOfRangeCylinderPower() {
+        this.validator.parseCylinderPower("100");
+    }
+
+    @Test
+    public void parseValidAxis() {
+        Assert.assertEquals(70, this.validator.parseAxis("x070"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void parseInvalidAxis() {
+        Assert.assertEquals(70, this.validator.parseAxis("070"));
+    }
+
+}
